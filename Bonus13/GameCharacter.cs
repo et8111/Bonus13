237608fcf43db6f47998f75c89287f23b2bc7749
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-24 13:00:17 -0400

    Add project files.

---
+++ Bonus13/GameCharacter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus13
+{
+    class GameCharacter
+    {
+        protected string Name { get; set; }
+        protected int Strength { get; set; }
+        protected int Intelligence { get; set; }
+
+        public GameCharacter(string name, int strength, int intelligence)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+        }
+
+        public virtual void Play()
+        {
+            Console.WriteLine($"Name: {Name}| Str: {Strength}| Int: {Intelligence}");
+        }
+    }
+
+    class Warrior : GameCharacter
+    {
+        public string weaponType { get; set; }
+        public Warrior(string name, int strength, int intelligence, string weapon): base(name, strength, intelligence)
+        {
+            weaponType = weapon;
+        }
+        public override void Play()
+        {
+            Console.WriteLine($"Name: {Name}| Str: {Strength}| Int: {Intelligence}| Weapon: {weaponType}");
+        }
+    }
+
+    class MagicUsingCharacter: GameCharacter
+    {
+        public int magicEnergy { get; set; }
+        public MagicUsingCharacter(string name, int strength, int intelligence, int ENERGY): base(name, strength, intelligence)
+        {
+            magicEnergy = ENERGY;
+        }
+        public override void Play()
+        {
+            Console.WriteLine($"Name: {Name}| Str: {Strength}| Int: {Intelligence}| MgcEng: {magicEnergy}");
+        }
+    }
+
+    class Wizard : MagicUsingCharacter
+    {
+        public int spellNumber { get; set; }
+
+        public Wizard(string name, int strength, int intelligence, int spellnum, int ENERGY): base(name, strength,intelligence, ENERGY)
+        {
+            spellNumber = spellnum;
+        }
+        public override void Play()
+        {
+            Console.WriteLine($"Name: {Name}| Str: {Strength}| Int: {Intelligence}| MgcEng: {magicEnergy}| SpellNumber: {spellNumber}");
+
+        }
+    }
+}
---
+++ Bonus13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameCharacter[] characters = new GameCharacter[5] {new Warrior("GRONK", 20, 2, "FIST"), new Warrior("Kevin", 11,9,"Dager"), new Wizard("Marin",2,19,5,11),new Wizard("Bradley Beal",9,9,3,9),new Wizard("BARRY THE BATTLE WIZARD",18,6,8,8)};
+            foreach (var v in characters)
+                v.Play();
+        }
+    }
+}
